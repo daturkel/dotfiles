88cb8059c587316522047e7e17530eb72bfb1a79
Dan Turkel <daturkel@gmail.com>
2023-01-03 14:36:35 -0500

    working lua config

---
+++ nvim/.config/nvim/lua/completion.lua
@@ -0,0 +1,81 @@
+vim.opt.completeopt = {"menu","menuone","preview"}
+
+-- Set up nvim-cmp.
+local cmp = require'cmp'
+
+
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+cmp.setup({
+  snippet = {
+    -- REQUIRED - you must specify a snippet engine
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+    end,
+  },
+  window = {
+    -- completion = cmp.config.window.bordered(),
+    -- documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<Tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end,
+    ['<S-Tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end,
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    -- ['<Esc>'] = cmp.mapping.close(),
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'path' },
+    { name = 'luasnip' }, -- For luasnip users.
+  }, {
+    { name = 'buffer' },
+  }),
+  completion = {
+      autocomplete = false
+  }
+})
+
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  }),
+  completion = {
+    autocomplete = false
+  }
+})
--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,42 +1,65 @@
-vim.cmd([[
-function! DoRemote(arg)
-    UpdateRemotePlugins
-endfunction
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
 
-call plug#begin('~/.config/nvim_plugins')
-    " start-page for vim
-    Plug 'mhinz/vim-startify'
-    " comment current line with `gcc`; visual mode with `gc`
-    Plug 'tpope/vim-commentary' 
-    " better `.` for repeating last commend
-    Plug 'tpope/vim-repeat'
-    " minimal statusline
-    Plug 'itchyny/lightline.vim'
-    " latex-related stuff
-    Plug 'lervag/vimtex'
-    " python syntax linter
-    Plug 'psf/black', { 'branch': 'stable' }
-    " deletion operations don't use the yank register
-    Plug 'svermeulen/vim-cutlass'
-    " TOML syntax
-    Plug 'cespare/vim-toml'
-    " COC completion
-    Plug 'neoclide/coc.nvim', {'branch': 'release'}
-    " FZF
-    Plug '/usr/local/opt/fzf'
-    Plug 'junegunn/fzf.vim'
-    " Indent guides
-    Plug 'nathanaelkane/vim-indent-guides'
-    " Richer markdown support
-    Plug 'godlygeek/tabular'
-    Plug 'plasticboy/vim-markdown'
-    " Git
-    Plug 'tpope/vim-fugitive'
-    " Wiki
-    Plug 'lervag/wiki.vim'
-    " vem dark theme
-    Plug 'pacha/vem-dark'
-    " Documentation generator
-    Plug 'fatih/vim-go'
-call plug#end()
-]])
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+  -- start-page for vim
+  use 'mhinz/vim-startify'
+  -- comment current line with `gcc`; visual mode with `gc`
+  use 'tpope/vim-commentary' 
+  -- better `.` for repeating last commend
+  use 'tpope/vim-repeat'
+  -- minimal statusline
+  use 'itchyny/lightline.vim'
+  -- latex
+  use 'lervag/vimtex'
+  -- python formatting
+  use 'psf/black'
+  -- delete operations don't use the yank register
+  use 'svermeulen/vim-cutlass'
+  -- toml syntax
+  use 'cespare/vim-toml'
+  -- -- code completion
+  -- use {'neoclide/coc.nvim', branch = "release"}
+  -- lsp
+  use 'neovim/nvim-lspconfig'
+  -- lsp completion
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+  use 'hrsh7th/nvim-cmp'
+  use 'L3MON4D3/LuaSnip'
+  use 'saadparwaiz1/cmp_luasnip'
+  use 'ray-x/lsp_signature.nvim'
+  -- fzf
+  use { 'junegunn/fzf', run = ":call fzf#install()" }
+  -- indent guides (tab to show)
+  use 'nathanaelkane/vim-indent-guides'
+  -- markdown
+  use 'godlygeek/tabular'
+  use 'plasticboy/vim-markdown'
+  -- git
+  use 'tpope/vim-fugitive'
+  -- wiki
+  use 'lervag/wiki.vim'
+  -- colorscheme
+  use 'pacha/vem-dark'
+  -- golang
+  use 'fatih/vim-go'
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
