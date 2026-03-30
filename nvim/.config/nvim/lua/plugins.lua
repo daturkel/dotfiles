e8f78053caefed15426cdbfe9453fc2e6ccf795b
Dan Turkel <daturkel@gmail.com>
2026-03-30 14:07:34 -0400

    Step 11: configure treesitter with highlight and indent

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -104,6 +104,15 @@ require("lazy").setup({
     end
   },
   { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
-  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+  { "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = { "python", "go", "lua", "bash", "json", "yaml", "markdown", "markdown_inline" },
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end
+  },
   { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 })
