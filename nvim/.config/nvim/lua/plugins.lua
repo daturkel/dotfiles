780742b34c1f58822c578424df6bcd6bd08b087b
Dan Turkel <daturkel@gmail.com>
2026-03-30 13:08:53 -0400

    Step 1: migrate plugin manager from packer to lazy.nvim

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,81 +1,39 @@
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({ "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
+vim.opt.rtp:prepend(lazypath)
 
-local packer_bootstrap = ensure_packer()
-
-return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
-  -- start-page for vim
-  use 'mhinz/vim-startify'
-  -- comment current line with `gcc`; visual mode with `gc`
-  use 'tpope/vim-commentary' 
-  -- better `.` for repeating last commend
-  use 'tpope/vim-repeat'
-  -- minimal statusline
-  use 'itchyny/lightline.vim'
-  -- colorschemes
-  use 'pacha/vem-dark'
-  -- latex
-  use 'lervag/vimtex'
-  -- python formatting
-  use 'psf/black'
-  -- delete operations don't use the yank register
-  use 'svermeulen/vim-cutlass'
-  -- toml syntax
-  use 'cespare/vim-toml'
-  -- -- code completion
-  use {'neoclide/coc.nvim', branch = 'release' }
-  -- indent guides (tab to show)
-  use 'nathanaelkane/vim-indent-guides'
-  -- markdown
-  use 'godlygeek/tabular'
-  use 'preservim/vim-markdown'
-  -- git
-  use 'tpope/vim-fugitive'
-  -- wiki
-  use 'lervag/wiki.vim'
-  -- golang
-  use 'fatih/vim-go'
-  -- keymap popup
-  use {
-    "folke/which-key.nvim",
-      config = function()
-        vim.cmd([[
-          set timeout
-          set timeoutlen=500
-        ]])
-        require("which-key").setup({plugins = { spelling = { enabled = true } } })
-     end
-  }
-  -- Auto highlight/unhighlight search results
-  use {
-    "asiryk/auto-hlsearch.nvim",
-      config = function()
-        require("auto-hlsearch").setup()
-      end
-  }
-  -- finder
-  use {
-    'nvim-telescope/telescope.nvim',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate'
-  }
-  use 'fannheyward/telescope-coc.nvim'
-  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
-end)
+require("lazy").setup({
+  { "tpope/vim-commentary" },
+  { "tpope/vim-repeat" },
+  { "svermeulen/vim-cutlass" },
+  { "cespare/vim-toml" },
+  { "godlygeek/tabular" },
+  { "preservim/vim-markdown" },
+  { "tpope/vim-fugitive" },
+  { "fatih/vim-go" },
+  { "lervag/wiki.vim" },
+  { "lervag/vimtex", ft = "tex" },
+  { "mhinz/vim-startify" },
+  { "psf/black", ft = "python" },
+  { "itchyny/lightline.vim" },
+  { "nathanaelkane/vim-indent-guides" },
+  { "pacha/vem-dark", priority = 1000 },
+  { "neoclide/coc.nvim", branch = "release" },
+  { "asiryk/auto-hlsearch.nvim", config = function()
+      require("auto-hlsearch").setup()
+    end
+  },
+  { "folke/which-key.nvim", config = function()
+      vim.opt.timeout = true
+      vim.opt.timeoutlen = 500
+      require("which-key").setup({ plugins = { spelling = { enabled = true } } })
+    end
+  },
+  { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
+  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+  { "fannheyward/telescope-coc.nvim" },
+  { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+})
