b7d46771d723fd9d2fc488e5cfca5ee39faec4e9
Dan Turkel <daturkel@gmail.com>
2026-03-30 13:27:59 -0400

    Fix indent-blankline: use background color style to match old vim-indent-guides look

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -53,7 +53,12 @@ require("lazy").setup({
   { "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
     config = function()
-      require("ibl").setup({ indent = { char = "│" } })
+      local highlight = { "CursorColumn", "Whitespace" }
+      require("ibl").setup({
+        indent = { highlight = highlight, char = "" },
+        whitespace = { highlight = highlight, remove_blankline_trail = false },
+        scope = { enabled = false },
+      })
       vim.keymap.set("n", "<C-i>", ":IBLToggle<CR>", { silent = true, desc = "Toggle indent guides" })
     end
   },
