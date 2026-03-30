8944698d32ae3c098f074b21b6a70a26499a7c5b
Dan Turkel <daturkel@gmail.com>
2026-03-30 13:19:12 -0400

    Step 4: replace vim-commentary with Comment.nvim

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -6,7 +6,7 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  { "tpope/vim-commentary" },
+  { "numToStr/Comment.nvim", config = function() require("Comment").setup() end },
   { "tpope/vim-repeat" },
   { "svermeulen/vim-cutlass" },
   { "cespare/vim-toml" },
